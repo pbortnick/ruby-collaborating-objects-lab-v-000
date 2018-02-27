aacb297002c5c193e20931e4547e4c3c9078fb7e
Pamela Bortnick <pbortnick@gmail.com>
2018-02-27 03:35:39 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,12 +6,27 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1]
+    new_song = self.new(song_name)
+    new_song.artist_name = artist_name
     new_song
   end
 
+# def self.new_by_filename(file)
+#       parts = file.split(" - ")
+#       artist_name = parts[0]
+#       song_name = parts[1]
+#       #genre_name = parts[2].gsub(".mp3", "")
+
+#       song = self.new(song_name)
+#       song.artist = artist_name
+#       song
+
+#       # a = Artist.find_or_create_by_name(artist_name)
+#       # a.add_song(self.new(song_name))
+#   end
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
